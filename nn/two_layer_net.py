a96c957935cd2294b413e9a92d90aa674f36fceb
110chang <u@110chang.com>
2018-03-11 14:48:58 +0900

    Add gradient check

--- nn/two_layer_net.py
+++ nn/two_layer_net.py
@@ -1,5 +1,7 @@
 from functions import *
 from gradients import numerical_gradient
+from layers import *
+from collections import OrderedDict
 
 
 class TwoLayerNet:
@@ -11,27 +13,32 @@ class TwoLayerNet:
             'W2': weight_init_std * np.random.randn(hidden_size, output_size),
             'b2': np.zeros(output_size),
         }
+        # レイヤーの生成
+        self.layers = OrderedDict()
+        self.layers = {
+            'Affine1': Affine(self.params['W1'], self.params['b1']),
+            'Relu1': Relu(),
+            'Affine2': Affine(self.params['W2'], self.params['b2']),
+        }
+        self.lastLayer = SoftmaxWithLoss()
 
     def predict(self, x):
-        W1, W2 = self.params['W1'], self.params['W2']
-        b1, b2 = self.params['b1'], self.params['b2']
+        for layer in self.layers.values():
+            x = layer.forward(x)
 
-        a1 = np.dot(x, W1) + b1
-        z1 = sigmoid(a1)
-        a2 = np.dot(z1, W2) + b2
-        y = softmax(a2)
-
-        return y
+        return x
 
     def loss(self, x, t):
         y = self.predict(x)
 
-        return cross_entropy_error(y, t)
+        return self.lastLayer.forward(y, t)
 
     def accuracy(self, x, t):
         y = self.predict(x)
         y = np.argmax(y, axis=1)
-        t = np.argmax(t, axis=1)
+
+        if (t.ndim != 1):
+            t = np.argmax(t, axis=1)
 
         accuracy = np.sum(y == t) / float(x.shape[0])
         return accuracy
@@ -43,6 +50,27 @@ class TwoLayerNet:
             'b1': numerical_gradient(loss_w, self.params['b1']),
             'W2': numerical_gradient(loss_w, self.params['W2']),
             'b2': numerical_gradient(loss_w, self.params['b2']),
+        }
+
+        return grads
+
+    def gradient(self, x, t):
+        self.loss(x, t)
+
+        dout = 1
+        dout = self.lastLayer.backward()
+
+        layers = list(self.layers.values())
+        layers.reverse()
+
+        for layer in layers:
+            dout = layer.backward(dout)
+
+        grads = {
+            'W1': self.layers['Affine1'].dW,
+            'b1': self.layers['Affine1'].db,
+            'W2': self.layers['Affine2'].dW,
+            'b2': self.layers['Affine2'].db,
         }
 
         return grads
